502bda46a19f578c821ef8ff4ac39a7fcf092370
nke-type0 <52362979+nke-type0@users.noreply.github.com>
2023-12-10 09:20:52 +0900

    add: Test UPM

---
+++ Scripts/TestScript.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace nke
+{
+    public class TestScript : MonoBehaviour
+    {
+
+        private void Start()
+        {
+            HelloPackage();
+        }
+
+
+        public void HelloPackage()
+        {
+            Debug.Log("Hello World");
+        }
+    }
+}
